36b696e741a5a8fc861803ad85dd157d6769d168
Grace Calianese <132861517+gcalianese@users.noreply.github.com>
2024-07-28 23:28:03 -0400

    add particle system and zombie die fx

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -2,78 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Author: Tarif Khan, Grace Calianese
-// This script serves for zombies to chase our player and attack them
 public class EnemyBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Transform player;
-    public float moveSpeed = 8;
-    public float minDistance = 0;
-    public AudioClip zombieHitSFX;
-    public AudioClip playerHitSFX;
-    //public AudioClip zombieDiesSFX;
-    public int startingHealth = 100;
-    int currentHealth;
+    public float moveSpeed = 10f;
+    public float minDistance = 0f;
+    public int damageAmount = 20;
 
-    int damageAmount = 20;
+    // Start is called before the first frame update
     void Start()
     {
-        if (player == null)
+        if(player == null)
         {
             player = GameObject.FindGameObjectWithTag("Player").transform;
         }
-        currentHealth = startingHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         float step = moveSpeed * Time.deltaTime;
-
         float distance = Vector3.Distance(transform.position, player.position);
         if (distance > minDistance)
         {
             transform.LookAt(player);
             transform.position = Vector3.MoveTowards(transform.position, player.position, step);
         }
-
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") && !LevelManager.isGameOver)
+        if(other.CompareTag("Player"))
         {
-            Debug.Log("Collision with player");
-            var playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            //apply damage
+            var playerHealth = other.GetComponent<PlayerHealth>();
             playerHealth.TakeDamage(damageAmount);
-            AudioSource.PlayClipAtPoint(playerHitSFX, Camera.main.transform.position);
-        }
 
-        if (other.gameObject.CompareTag("Projectile"))
-        {
-            Debug.Log("Projectile collision detected");
-            AudioSource.PlayClipAtPoint(zombieHitSFX, transform.position);
-            Destroy(other.gameObject);
-            TakeDamage(FindObjectOfType<ShootProjectile>().projectileDamage);
         }
-    }
-
-    void TakeDamage(int damageAmount)
-    {
-        currentHealth -= damageAmount;
-        damageAmount = Mathf.Clamp(currentHealth, 0, 100);
-        if(currentHealth <= 0)
-        {
-            DestroyZombie();
-        }
-    }
-
-    void DestroyZombie()
-    {
-        // This will be the particle system
-        // Instantiate(zombieDiesSFX, transform.position, transform.rotation);
-        gameObject.SetActive(false);
-        Destroy(gameObject, .5f);
     }
 }
